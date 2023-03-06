714b7c74161cdf6275f32f244f51f1fb5d4e875c
keymon2 <sg0814203@naver.com>
2023-03-06 13:35:47 +0900

    Initial Commit

---
+++ LoadingImage/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LoadingImage
+//
+//  Created by seung won lee on 2023/03/06.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
